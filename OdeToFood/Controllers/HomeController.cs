c830d5d173f18bc1d31793dd7caf95812b53b6a4
Zand <zand.p@excelbangkok.com>
2017-05-16 12:47:58 +0700

    Initial project

---
+++ OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            var model = new AboutModel();
+            model.Name = "Scott";
+            model.Location = "Maryland, USA";
+
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
