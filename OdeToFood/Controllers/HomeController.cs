e54fc99e04f05d711b839b8e4e11298bd6f47321
zabahey <zabahey.sukoi@gmail.com>
2017-05-16 19:05:28 +0700

    get value from route data

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -11,7 +11,13 @@ namespace OdeToFood.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            var controller = RouteData.Values["controller"];
+            var action = RouteData.Values["action"];
+            var id = RouteData.Values["id"];
+
+            var message = string.Format("{0}::{1} {2}", controller, action, id);
+
+            ViewBag.Message = message;
 
             return View();
         }
