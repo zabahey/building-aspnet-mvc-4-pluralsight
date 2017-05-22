26df4ebdd7558d867aeeb803526fb5ff1c085069
zabahey <zabahey.sukoi@gmail.com>
2017-05-22 18:07:17 +0700

    try return difference type of action result

--- OdeToFood/Controllers/CuisineController.cs
+++ OdeToFood/Controllers/CuisineController.cs
@@ -14,7 +14,7 @@ namespace OdeToFood.Controllers
         public ActionResult Search(string name = "french")
         {
             var message = Server.HtmlEncode(name);
-            return Content(message);
+            return Json(new { Message=message, Name="Scott" }, JsonRequestBehavior.AllowGet);
         }
 
     }
